00ade9a7dd17a5c61f65b5000bab768022d170ae
Luis Fernando Rodrigues Bueno <77luisnando@gmail.com>
2025-08-15 22:45:41 -0300

    Começando Exemplo 01

---
+++ src/exemplos/ex01/index.js
@@ -0,0 +1,10 @@
+import { View, Text } from 'react-native';
+
+export default function Exemplo01() {
+    return (
+        <View>
+            <Text>Exemplo 01</Text>
+            <Text>Introdução React Native com Expo</Text>
+        </View>
+    );
+}
