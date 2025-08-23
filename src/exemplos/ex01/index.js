18e195bcfaa585e87cf30b9e7a4062c0e72bec41
Luis Fernando Rodrigues Bueno <77luisnando@gmail.com>
2025-08-22 21:17:14 -0300

    Aula style

--- src/exemplos/ex01/index.js
+++ src/exemplos/ex01/index.js
@@ -1,9 +1,11 @@
-import { View, Text } from 'react-native';
+import { View, Text} from 'react-native';
 
-export default function Exemplo01() {
+import styles from './styles';
+
+export default function Exemplo01() {''
     return (
-        <View>
-            <Text>Exemplo 01</Text>
+        <View style={styles.container}>
+            <Text style={styles.titulo}>Exemplo 01</Text>
             <Text>Introdução React Native com Expo</Text>
         </View>
     );
