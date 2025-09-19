62fd26508faceb92883f9bbb167587663ae71a31
LUIS FERNANDO RODRIGUES BUENO <luis.bueno34>
2025-09-19 20:38:16 -0300

    Exemplo 4 function

--- src/exemplos/ex04/index.js
+++ src/exemplos/ex04/index.js
@@ -7,38 +7,40 @@ export default function Exemplo04() {
 
     const [texto, setTexto] = useState('');
     const [txt2, setTxt2] = useState('');
+    const [mensagem, setMensagem] = useState('');
+
+    function handleExibeMensagem() {
+        setMensagem(txt2);
+        setTxt2('');
+    }
 
     return (
         <View style={styles.container}>
             <Text style={styles.titulo}>Exemplo 04</Text>
 
             <Text style={styles.txt}>{texto}</Text>
-
             <TextInput
                 onChangeText={setTexto}
                 placeholder='Senha'
                 keyboardType='ascii-capable'
-                // secureTextEntry
+                secureTextEntry
                 style={styles.input}
             />
 
-            <Text style={styles.txt}>{txt2}</Text>
-
+            <Text style={styles.txt}>{mensagem}</Text>
             <TextInput
                 onChangeText={setTxt2}
                 placeholder='Digite seu número'
-                keyboardType='numeric'
+                keyboardType='default'
                 maxLength={5}
                 style={styles.input}
             />
 
-            <TouchableOpacity 
-            style={styles.botao} 
-            onPress={() => {}
-            }>
+            <TouchableOpacity
+                style={styles.botao}
+                onPress={() => handleExibeMensagem()}>
                 <Text style={styles.txtBotao}>Exibir Texto</Text>
             </TouchableOpacity>
-
         </View>
     );
 }
