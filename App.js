3059e9a9000c67b90e5573096ccb2c818ac325ea
Luis Fernando Rodrigues Bueno <77luisnando@gmail.com>
2025-09-29 21:17:58 -0300

    29/09/2025

--- App.js
+++ App.js
@@ -1,15 +1,16 @@
+import * as React from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TextInput} from 'react-native';
 import Constants from 'expo-constants';
 
-import Exemplo04 from './src/exemplos/ex04';
+import Index from './src/components/index';
 
 
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Exemplo04 />
+      <Index />
       <StatusBar style="light" />''
     </View>
   );
@@ -18,8 +19,9 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    justifyContent: 'center',
     backgroundColor: 'darkslategrey',
-    padding: 10,
+    padding: 8,
     paddingTop: Constants.statusBarHeight,
   },
 });
---
+++ src/components/styles.js
@@ -0,0 +1,44 @@
+import { StyleSheet } from 'react-native';
+import { RFPercentage } from "react-native-responsive-fontsize";
+
+const styles = StyleSheet.create(
+    {
+        container: {
+            backgroundcolor: 'white',
+            // alignItems: 'center',
+            // flex: 1,
+        },
+
+        txtSaida: {
+            margin: 24,
+            fontSize: 22,
+            fontWeight: 'bold',
+            textAlign: 'center',
+            color: '#E53935',
+        },
+
+        txtEntrada: {
+            borderWidth: 4,
+            textAlign: 'center',
+            fontSize: 22,
+            borderColor: '#B71C1C',
+            height: 40,
+            color: '#E53935',
+            borderRadius: 10,
+        },
+
+        button: {
+            backgroundColor: '#E53935',
+            height: 40,
+            justifyContent:'center',
+
+        },
+
+        textButton: {
+
+        },
+    }
+);
+
+
+export default styles;
