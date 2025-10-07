0d133692430e9c67b3dc619f8ce6cd4414d0fa07
Luis Fernando Rodrigues Bueno <77luisnando@gmail.com>
2025-10-06 22:46:28 -0300

    06/10

--- src/exemplos/ex05/index.js
+++ src/exemplos/ex05/index.js
@@ -6,7 +6,26 @@ import styles from './styles';
 export default function Exemplo05() {''
     return (
         <View style={styles.container}>
-            <Text style={styles.paragraph}>Exemplo 05</Text>
+            <Text style={styles.paragraph}> Exemplo 05 </Text>
+
+            <Text style={styles.txtSaida}>Calculadora básica</Text>
+
+            <Text style={styles.txtLabel}> 1º número </Text>
+            <TextInput style={styles.txtEntrada} />
+
+            <Text style={styles.txtSaida}> + </Text>
+
+            <Text style={styles.txtLabel}> 2º número </Text>
+            <TextInput style={styles.txtEntrada} />
+
+            <Text style={[styles.txtSaida, {margin: 0}]}> = </Text>
+
+            <Text style={styles.txtLabel}> Total </Text>
+            <TextInput style={styles.txtEntrada} />
+
+            <TouchableOpacity style={styles.button}>
+                <Text style={styles.txtButton}> + </Text>
+            </TouchableOpacity>
         </View>
     );
 }
--- src/exemplos/ex05/styles.js
+++ src/exemplos/ex05/styles.js
@@ -16,7 +16,46 @@ const styles = StyleSheet.create(
             fontWeight: 'bold',
             textAlign: 'center',
             color: '#C51162',
-        }
+        },
+
+        txtSaida: {
+            margin: 6,
+            fontSize: 22,
+            fontWeight: 'bold',
+            textAlign: 'center',
+            color: '#E91163',
+        },
+
+        txtEntrada: {
+            borderWidth: 4,
+            textAlign: 'center',
+            fontSize: 22,
+            borderColor: '#E91163',
+            height: 40,
+            color: '#E53935',
+            borderRadius: 10,
+            marginTop: 10,   
+        },
+
+        button: {
+            backgroundColor: '#E91163',
+            height: 40,
+            justifyContent: 'center',
+            borderRadius: 10,
+            marginTop: 20,
+        },
+
+        txtButton: {
+            fontSize: 22,
+            color: '#FF80AB',
+            textAlign: 'center',
+        },
+
+        txtLabel: {
+            fontSize: 16,
+            color: '#C51162',
+            fontWeight: 'bold',
+        },
     }
 );
 
